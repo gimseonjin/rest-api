da631fa60fec3c238b8b7c34d999b9863f8a42a0
gimseonjin <kimseonjin616@naver.com>
2022-03-21 23:21:25 +0900

    Build : Separation Test Yml Settings From Main Yml Settings
    테스트 DB 세팅을 메인 DB 세팅에서 분리했습니다.
    
    참고 : #

--- src/test/java/com/carrykim/restapi/event/EventControllerTest.java
+++ src/test/java/com/carrykim/restapi/event/EventControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.stream.IntStream;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class EventControllerTest {
 
     @Autowired
